f3a59034abc238236a501cba5da97de34a792434
RadoKopacha <petyrnikolov0804@gmail.com>
2024-12-05 21:37:34 +0200

    typa zadacha

---
+++ StackHomework 1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace StackHomework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            string word = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+           
+            foreach (char c in word)
+            {
+                stack.Push(c);
+            }
+
+            bool pale = true;             
+            foreach (char c in word)
+            {
+                if (stack.Pop() != c)
+                {
+                    pale = false;
+                    break;
+                }
+            }          
+            if (pale)
+            {
+                Console.WriteLine("is palendrom");
+            }
+            else
+            {
+                Console.WriteLine("it is not palendrom");
+            }            
+          
+        }
+    }
+}
